59cb204677c734aa1b7944b457903f28200431e8
gitonga403 <morris.gitonga@nextranssolutions.co.ke>
2025-02-14 11:47:19 +0300

    public interface enhancement

---
+++ developmentv2/web-application/public/views/dev_portal/comesa-sws/src/app/core-modules/public/views/import-procedures/import-procedures.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-import-procedures',
+
+  templateUrl: './import-procedures.component.html',
+  styleUrl: './import-procedures.component.css'
+})
+export class ImportProceduresComponent {
+
+}
---
+++ developmentv2/web-application/public/views/dev_portal/comesa-sws/src/app/core-modules/public/views/importexports-costs/importexports-costs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-importexports-costs',
+ 
+  templateUrl: './importexports-costs.component.html',
+  styleUrl: './importexports-costs.component.css'
+})
+export class ImportexportsCostsComponent {
+
+}
