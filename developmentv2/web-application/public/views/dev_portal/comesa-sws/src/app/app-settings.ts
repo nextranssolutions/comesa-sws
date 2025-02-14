982d550b940af5fb06a5e99ad9b421dc59928687
Nextrans Solutions Limited <146913215+nextranssolutions@users.noreply.github.com>
2025-02-14 14:18:06 +0300

    system Updates
    system Updates

--- developmentv2/web-application/public/views/dev_portal/comesa-sws/src/app/app-settings.ts
+++ developmentv2/web-application/public/views/dev_portal/comesa-sws/src/app/app-settings.ts
@@ -10,7 +10,7 @@ if (window.location.hostname == 'localhost') {
 }
 if (app_connection == 'development') {
   //nextrans-solutions\comesa-sws\development\web-application
-  base_url = 'http://localhost/nextrans-solutions/comesa-sws/developmentv2/web-application/public/index.php';
+  base_url = 'http://localhost:82/nextrans-solutions/comesa-sws/developmentv2/web-application/public/index.php';
   assets_url = 'http://localhost/nextrans-solutions/comesa-sws/development2/web-application/public/index.php';
   // siteKey = '6LcoH54UAAAAAOqpAGCXC4cmup6N2c5KseVHmv1c';
 }
