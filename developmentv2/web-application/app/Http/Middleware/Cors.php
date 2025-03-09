e2c869a95147656bee2706deaee51f1c57816b22
Nextrans Solutions Limited <146913215+nextranssolutions@users.noreply.github.com>
2025-03-09 21:42:01 +0300

    Update

--- developmentv2/web-application/app/Http/Middleware/Cors.php
+++ developmentv2/web-application/app/Http/Middleware/Cors.php
@@ -7,9 +7,11 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Authorization, Content-Type, X-Requested-With');
+        $response = $next($request);
+            $response->header('Access-Control-Allow-Origin', '*');
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
+            return $response;
+     
     }
 }
